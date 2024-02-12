8534f45d2285125cc6024d587a83809f873b7bbf
Saurabh <saurabkumar679@gmail..com>
2024-02-12 22:17:12 +0530

    mui designing  done

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,5 +1,82 @@
 import Layout from "../../components/Layout/Layout";
-
+import { Box, Typography } from "@mui/material";
 export default function About() {
-  return <Layout>About us</Layout>;
+  return (
+    <Layout>
+      <Box
+        sx={{
+          my: 4,
+          "& h4": {
+            fontWeight: "bold",
+            textAlign: "center",
+          },
+          "& p": {
+            textAlign: "justify",
+          },
+        }}
+      >
+        <Typography variant="h4">About us</Typography>
+        <Typography sx={{ padding: "10px" }}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat iure
+          libero ab nobis voluptatum sequi. Consequuntur itaque voluptate
+          praesentium, recusandae a molestiae iste explicabo in aperiam,
+          temporibus possimus, sequi doloribus? Minus impedit, harum at deleniti
+          blanditiis illum dolore natus quibusdam veritatis ex consequuntur
+          autem repellat? Laboriosam, nisi reprehenderit itaque dicta excepturi
+          quisquam veniam dolore atque! Laudantium neque recusandae provident
+          magni dolor dolorem sit quae, fuga impedit. Animi omnis dolores
+          debitis dolorem iste doloremque ratione, tempora a doloribus nemo,
+          distinctio facilis labore. Iste repudiandae impedit laborum! Ipsam
+          omnis blanditiis esse molestiae iusto sapiente voluptatibus iure?
+          Minima odio adipisci fugit nobis consequatur assumenda natus, eaque
+          nesciunt corporis eius, earum perferendis voluptate officiis est
+          aliquam! Dicta eveniet ducimus aut ipsum ad perspiciatis harum odio
+          cumque provident modi, optio ipsa officia quidem quo cupiditate
+          ratione inventore eos quaerat deleniti. Praesentium animi doloribus
+          accusamus voluptates ullam ipsa dolore, sapiente asperiores iste,
+          autem odit tempora aut eum repellat natus assumenda delectus vitae,
+          veniam cupiditate neque commodi aliquid qui dolores mollitia! Adipisci
+          sint vitae id, aliquid porro ipsum possimus ipsa consectetur rerum
+          corrupti voluptates quod accusantium, minus voluptatum suscipit illum
+          sapiente! Id esse consectetur molestiae quos similique magnam
+          architecto totam voluptas commodi, itaque expedita. Magnam voluptas
+          modi dolore consequatur non. Itaque rem ullam nisi voluptate! Libero
+          accusamus labore delectus necessitatibus nam nisi, quo neque nulla
+          sint ratione! Natus, ducimus. Soluta minus deleniti, neque quam rem ab
+          aut magni earum sed nisi et id reiciendis sint consequuntur voluptas
+          facere fugit saepe quasi ea cum placeat? Repellat voluptas veritatis
+          maiores? Necessitatibus impedit voluptatum expedita? Nisi consequuntur
+          illum incidunt veniam, temporibus excepturi esse aliquam earum
+          dignissimos nulla? Ex placeat, officia nobis accusamus aperiam
+          reprehenderit, dolorem numquam dicta non praesentium optio velit
+          adipisci doloribus ipsum, iure soluta blanditiis? Velit consequatur
+          voluptatibus commodi quibusdam molestias maxime, sapiente sequi
+          praesentium ipsa cumque iusto neque! Ducimus sequi expedita voluptatum
+          assumenda iure repellendus similique nisi rerum, ut inventore
+          consectetur asperiores? Nihil sequi accusantium ex debitis deleniti
+          vel nobis doloremque est. Animi officiis amet explicabo! Cupiditate
+          ducimus aspernatur temporibus quo perspiciatis assumenda voluptatem
+          architecto ut dolorem iste rerum consequuntur eius, blanditiis
+          molestias consectetur incidunt omnis debitis saepe labore beatae hic
+          unde provident mollitia? Dignissimos suscipit animi at dolore
+          cupiditate recusandae aut architecto expedita soluta laudantium.
+          Recusandae natus quam minima id ipsam, similique veniam quis
+          praesentium ipsum accusantium nisi provident quibusdam dolor dicta
+          distinctio aspernatur quas animi sequi unde. In reiciendis laborum
+          quibusdam sapiente illo repellendus tenetur exercitationem, quisquam
+          explicabo sed porro facere molestias dolor corrupti! Minima, dicta
+          necessitatibus aliquid nemo voluptatem quos rem reiciendis eos vel
+          libero reprehenderit tempore praesentium at nisi quae nesciunt autem
+          perspiciatis quam placeat ullam in molestias omnis consequuntur
+          soluta! Praesentium, quam. Deleniti labore nihil architecto reiciendis
+          rerum optio ducimus quaerat pariatur, accusamus deserunt, praesentium
+          neque quis voluptatum enim cupiditate hic commodi, debitis cum odio
+          aut molestias fugit voluptates rem repellat? Dolore numquam qui natus!
+          Rem sunt, possimus excepturi quidem unde quasi quis at exercitationem
+          libero cumque, neque soluta! Ullam, dignissimos numquam. Excepturi ea
+          qui consectetur. Nemo facilis ex at voluptates quod.
+        </Typography>
+      </Box>
+    </Layout>
+  );
 }
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,93 @@
 import Layout from "../../components/Layout/Layout";
+import SupportAgentIcon from "@mui/icons-material/SupportAgent";
+import EmailIcon from "@mui/icons-material/Email";
+import CallIcon from "@mui/icons-material/Call";
+import {
+  Box,
+  Typography,
+  TableContainer,
+  Paper,
+  Table,
+  TableRow,
+  TableCell,
+  TableHead,
+  TableBody,
+} from "@mui/material";
+
 export default function Contact() {
   return (
     <Layout>
-      <div>Contact</div>
+      <div>
+        <Box
+          sx={{
+            my: 10,
+            // ml: 10,
+            "& h4": {
+              textAlign: "center",
+              fontWeight: "bold",
+              mb: 2,
+            },
+          }}
+        >
+          <Typography variant="h4">Contact us</Typography>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+            quis aliquam labore officiis enim! Consectetur molestiae, commodi
+            est quae voluptatum aperiam impedit sed fugiat deleniti vero optio
+            laborum, ducimus, molestias nisi dolores. Cupiditate ullam cumque
+            amet autem libero doloribus voluptatum qui voluptatem, hic non
+            labore, ad quos quae, illo aperiam molestiae. Reiciendis doloremque
+            itaque omnis architecto incidunt fuga quisquam rerum necessitatibus!
+            Harum aspernatur minus consequuntur magni recusandae voluptas illum
+            totam repellendus ex. Ratione debitis dignissimos eligendi
+            asperiores explicabo doloremque natus illum impedit. Earum, mollitia
+            molestias. Iste optio accusantium officia, magnam eius modi
+            consequuntur porro perspiciatis enim, deserunt ut sapiente
+            recusandae.
+          </p>
+        </Box>
+        <Box sx={{ padding: 3 }}>
+          <TableContainer component={Paper}>
+            <Table aria-label="contact label">
+              <TableHead>
+                <TableRow>
+                  <TableCell
+                    sx={{
+                      textAlign: "center",
+                      backgroundColor: "black",
+                      color: "white",
+                    }}
+                  >
+                    Contact Detail
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell>
+                    <SupportAgentIcon sx={{ color: "red", pt: 1 }} /> 123456789
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    <EmailIcon sx={{ color: "skyblue", pt: 1 }} /> 123456789
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    <CallIcon sx={{ color: "green", pt: 1 }} /> 123456789
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    <CallIcon sx={{ color: "green", pt: 1 }} /> 123456789
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
+      </div>
     </Layout>
   );
 }
