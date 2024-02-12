3d0436ddda8d38b77a95f4a0976c2a9f63371192
Saurabh <saurabkumar679@gmail..com>
2024-02-12 15:20:32 +0530

    hero section done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,47 @@
 import { Box, Typography } from "@mui/material";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+
 export default function Footer() {
   return (
     <>
       <Box
         sx={{
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: "gray",
-          display: "flex",
+          textAlign: "center",
+          backgroundColor: "#1A1A19",
+          color: "white",
+          padding: 3,
         }}
       >
-        <Typography variant="h6">
+        <Box
+          sx={{
+            "& svg": {
+              fontSize: "60px",
+              marginRight: 2,
+              cursor: "pointer",
+            },
+            "& svg:hover": {
+              color: "goldenrod",
+              transform: "translateX(5px)",
+              transition: "all 400ms",
+            },
+          }}
+        >
+          <InstagramIcon />
+          <TwitterIcon />
+          <GitHubIcon />
+          <YouTubeIcon />
+        </Box>
+        <Typography
+          variant="h6"
+          sx={{
+            "@media (max-width:600px)": {
+              fontSize: "1 rem",
+            },
+          }}
+        >
           All Rights Reservred &copy; Saurabh
         </Typography>
       </Box>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,23 @@
 import Layout from "../../components/Layout/Layout";
+import { Link } from "react-router-dom";
+import banner from "../../images/banner.jpeg";
+import "./Home.css";
 
 export default function Home() {
   return (
     <Layout>
-      <h1>Home</h1>
+      <div
+        className="home"
+        style={{ backgroundImage: `url(${banner})`, objectFit: "cover" }}
+      >
+        <div className="headercontainer">
+          <h1>Food Website</h1>
+          <p>Best Food in India</p>
+          <Link to="/menu">
+            <button>Order Now</button>
+          </Link>
+        </div>
+      </div>
     </Layout>
   );
 }
